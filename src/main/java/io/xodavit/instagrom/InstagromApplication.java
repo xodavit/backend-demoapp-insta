c1bb2a78ccd3aaf932ad2a31ea1182ddc03c240b
vitaliy_khodataev <khodataev.v@yandex.ru>
2021-03-04 05:10:53 +0300

    spring initializr dependency: spring data jpa, spring security, rest repositories, validation, mySqlDrier, Lombok

---
+++ src/main/java/io/xodavit/instagrom/InstagromApplication.java
@@ -0,0 +1,13 @@
+package io.xodavit.instagrom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagromApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagromApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/xodavit/instagrom/InstagromApplicationTests.java
@@ -0,0 +1,13 @@
+package io.xodavit.instagrom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagromApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

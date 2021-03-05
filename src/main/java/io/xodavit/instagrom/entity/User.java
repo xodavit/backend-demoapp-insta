0f7d339618a649b5a65d40b2f5dcdccc55e506a3
vitaliy_khodataev <khodataev.v@yandex.ru>
2021-03-05 20:33:45 +0300

    add variables to user

--- src/main/java/io/xodavit/instagrom/entity/User.java
+++ src/main/java/io/xodavit/instagrom/entity/User.java
@@ -1,4 +1,27 @@
 package io.xodavit.instagrom.entity;
 
+import io.xodavit.instagrom.entity.enums.ERole;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class User {
+    private Long id;
+    private String name;
+    private String username;
+    private String lastname;
+    private String email;
+    private String bio;
+    private String password;
+
+    private Set<ERole> role = new HashSet<>();
+    private List<Post> posts = new ArrayList<>();
+    private LocalDateTime createdDate;
+
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+    }
 }

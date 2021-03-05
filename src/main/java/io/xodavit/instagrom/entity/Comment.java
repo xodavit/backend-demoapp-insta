db4c034cdeb80491f9acf2cf4c61798f8bd9aa07
vitaliy_khodataev <khodataev.v@yandex.ru>
2021-03-05 20:47:43 +0300

    add variables to comment model

--- src/main/java/io/xodavit/instagrom/entity/Comment.java
+++ src/main/java/io/xodavit/instagrom/entity/Comment.java
@@ -1,4 +1,16 @@
 package io.xodavit.instagrom.entity;
 
+import java.time.LocalDateTime;
+
 public class Comment {
+    private Long id;
+    private Post post;
+    private String username;
+    private Long userId;
+    private String message;
+    private LocalDateTime createdDate;
+
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+    }
 }

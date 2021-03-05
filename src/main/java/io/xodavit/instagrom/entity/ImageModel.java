577a1542ccb0828b61035eee2fbd06f401969888
vitaliy_khodataev <khodataev.v@yandex.ru>
2021-03-05 20:44:47 +0300

    add variables to image model

--- src/main/java/io/xodavit/instagrom/entity/ImageModel.java
+++ src/main/java/io/xodavit/instagrom/entity/ImageModel.java
@@ -1,4 +1,11 @@
 package io.xodavit.instagrom.entity;
 
 public class ImageModel {
+    private Long id;
+    private String name;
+    private byte[] imageBytes; //blob
+    //нету прямой зависимости
+    private Long userId;
+    private Long postId;
+
 }

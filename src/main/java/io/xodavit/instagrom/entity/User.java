f37934d75dba00dac2c2944dd9f940b8200ec2c0
vitaliy_khodataev <khodataev.v@yandex.ru>
2021-03-07 23:08:51 +0300

    add annotation to user model @lombok @entity @column

--- src/main/java/io/xodavit/instagrom/entity/User.java
+++ src/main/java/io/xodavit/instagrom/entity/User.java
@@ -1,25 +1,42 @@
 package io.xodavit.instagrom.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.xodavit.instagrom.entity.enums.ERole;
+import lombok.Data;
 
-import javax.persistence.PrePersist;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(unique = true, updatable = false)
     private String username;
+    @Column(nullable = false)
     private String lastname;
+    @Column(unique = true)
     private String email;
+    @Column(columnDefinition = "text")
     private String bio;
+    @Column(length = 3000)
     private String password;
-
-    private Set<ERole> role = new HashSet<>();
+    @ElementCollection(targetClass = ERole.class) //one to many, через создание новой таблицы
+    @CollectionTable(name = "user_role",
+    joinColumns = @JoinColumn("user_id"))
+    private Set<ERole> roles = new HashSet<>();
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
     private List<Post> posts = new ArrayList<>();
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
     private LocalDateTime createdDate;
 
     @PrePersist

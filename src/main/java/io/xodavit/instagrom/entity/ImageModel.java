051b121bf842c68415b8c70bae14e49eccfc4e33
vitaliy_khodataev <khodataev.v@yandex.ru>
2021-03-07 23:49:55 +0300

    add annotation for image model lombok @data data @Entity, @Column

--- src/main/java/io/xodavit/instagrom/entity/ImageModel.java
+++ src/main/java/io/xodavit/instagrom/entity/ImageModel.java
@@ -1,11 +1,25 @@
 package io.xodavit.instagrom.entity;
 
+import lombok.Data;
+import net.minidev.json.annotate.JsonIgnore;
+
+import javax.persistence.*;
+
+@Data
+@Entity
 public class ImageModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
-    private byte[] imageBytes; //blob
-    //нету прямой зависимости
+    @Lob
+    @Column(columnDefinition = "LONGBLOB")
+    private byte[] imageBytes;
+    @JsonIgnore //нету прямой зависимости,и мы сделаем свои dto, поэтому не сохранем в базу
     private Long userId;
+    @JsonIgnore //нету прямой зависимости,и мы сделаем свои dto, поэтому не сохранем в базу
     private Long postId;
 
 }

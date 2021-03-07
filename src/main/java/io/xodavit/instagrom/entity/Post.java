076178a42fa2d414afb41cd44782ebc3cdd26566
vitaliy_khodataev <khodataev.v@yandex.ru>
2021-03-07 23:30:33 +0300

    add annotation for post model lombok @data data @Entity, @Column

--- src/main/java/io/xodavit/instagrom/entity/Post.java
+++ src/main/java/io/xodavit/instagrom/entity/Post.java
@@ -1,22 +1,35 @@
 package io.xodavit.instagrom.entity;
 
-import javax.persistence.PrePersist;
+import lombok.Data;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@Data
+@Entity
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String caption;
     private String location;
     private Integer likes;
 
+    @Column
+    @ElementCollection(targetClass = String.class)//чтоб мы могли сохранять текстовую коллекцию пользователей
     private Set<String> likedUsers = new HashSet<>();
+    @ManyToOne(fetch = FetchType.LAZY)//со стороны пользователя у нас onetomany, но со стороны поста у нас manytoone
+    //у одного поста только один пользователь
     private User user;
+    @OneToMany(cascade = CascadeType.REFRESH, fetch =  FetchType.EAGER, mappedBy = "post", orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
+    @Column(updatable = false)
     private LocalDateTime createdDate;
 
     @PrePersist

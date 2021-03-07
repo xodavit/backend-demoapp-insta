fe03adef090d1068166c290c83395be2a050cc5e
vitaliy_khodataev <khodataev.v@yandex.ru>
2021-03-07 23:40:39 +0300

    add annotation for comment model lombok @data data @Entity, @Column

--- src/main/java/io/xodavit/instagrom/entity/Comment.java
+++ src/main/java/io/xodavit/instagrom/entity/Comment.java
@@ -1,14 +1,26 @@
 package io.xodavit.instagrom.entity;
 
-import javax.persistence.PrePersist;
+import lombok.Data;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Data
+@Entity
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne(fetch = FetchType.EAGER) //у одного комментария может быть только один пост
     private Post post;
+    @Column(nullable = false)
     private String username;
+    @Column(nullable = false)
     private Long userId;
+    @Column(columnDefinition = "text", nullable = false)
     private String message;
+    @Column(updatable = false)
     private LocalDateTime createdDate;
 
     @PrePersist

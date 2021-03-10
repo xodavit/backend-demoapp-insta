4e5638b54225bf49957550ae250d283e0cf1a418
vitaliy_khodataev <khodataev.v@yandex.ru>
2021-03-10 21:56:49 +0300

    removed unused imports

--- src/main/java/io/xodavit/instagrom/service/CustomUserDetailsService.java
+++ src/main/java/io/xodavit/instagrom/service/CustomUserDetailsService.java
@@ -1,15 +1,12 @@
 package io.xodavit.instagrom.service;
 
-import io.xodavit.instagrom.entity.User;
 import io.xodavit.instagrom.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {

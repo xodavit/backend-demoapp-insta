9cebaebf582c2ca002ad4e9a8330c3b4b0f2aaf6
vitaliy_khodataev <khodataev.v@yandex.ru>
2021-03-10 21:55:45 +0300

    add new package service; add new CustomUserDetailsService class in service package; add annotation @Service in CustomUserDetailsService; * implements UserDetailsService method return null; * add private final UserRepository; * adding a CustomUserDetailsService constructor with 1 parameter UserRepository; *add dependency injection in constructor annotation @Autowired;

---
+++ src/main/java/io/xodavit/instagrom/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package io.xodavit.instagrom.service;
+
+import io.xodavit.instagrom.entity.User;
+import io.xodavit.instagrom.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    //класс создан для секьюрити конфига.
+    // authentication manager builder будет использовать методы этого класса и доставать данные из
+
+    private final UserRepository userRepository;
+
+    //внедрение зависимости через конструктор
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
+
+
+}
